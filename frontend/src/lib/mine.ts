18f5fb14ecbdb54099581ff00d73cf4f3b857030
Josh Hoover <caboodlennm@gmail.com>
2023-09-26 12:52:00 -0500

    Shader refactoring

--- frontend/src/lib/mine.ts
+++ frontend/src/lib/mine.ts
@@ -13,7 +13,9 @@ export const mine = (
     "mining prompt: ",
     prompt,
     " target: ",
-    target.toString("hex").padStart(64, "0")
+    target.toString("hex").padStart(64, "0"),
+    " prevHash: ",
+    currentHash
   );
 
   let block: Block = {
@@ -44,61 +46,40 @@ export const mine = (
     block.nonce.iaddn(1);
   }
 
-  // const nonceBytes = Buffer.from(block.nonce.toArray());
-  const nonceBytes = Buffer.from(new BN(0).toArray("be", 32));
-  console.log("nonceBytes: ", nonceBytes.toString("hex"));
-
   const prevHashBytes = Buffer.from(block.prevHash, "hex");
   const promptBytes = Buffer.from(block.prompt, "utf8");
   const responseBytes = Buffer.from(block.prevResponse, "utf8");
 
   const length =
-    nonceBytes.length +
-    prevHashBytes.length +
-    promptBytes.length +
-    responseBytes.length;
+    // nonceBytes.length +
+    prevHashBytes.length + promptBytes.length + responseBytes.length;
 
   // pad buffer to 4 byte boundary with 0s
   const padding = 4 - (length % 4);
   const paddingBytes = Buffer.alloc(padding);
 
   const buffer = Buffer.concat([
-    nonceBytes,
+    // nonceBytes,
     prevHashBytes,
     promptBytes,
     responseBytes,
     paddingBytes,
   ]);
 
-  // hash buffer
-  testHash(buffer);
+  process_block(buffer)
+    .then((hashes) => {
+      for (let i = 0; i < hashes.length; i += 32) {
+        console.log(
+          hashes
+            .subarray(i, i + 32)
+            .reduce((a, b) => a + b.toString(16).padStart(2, "0"), "")
+        );
+      }
+    })
+    .catch(console.error);
 };
 
-async function testHash(bytes: Buffer) {
-  // const messages = [
-  // new Uint8Array([0x01, 0x00, 0x00, 0x00]), // int 1
-  // new Uint8Array([0x02, 0x00, 0x00, 0x00]), // int 2
-  // new Uint8Array([0x03, 0x00, 0x00, 0x00]), // int 3
-  // new Uint8Array([0x04, 0x00, 0x00, 0x00]), // int 4
-  // new Uint8Array([0x05, 0x00, 0x00, 0x00]), // int 5
-  // new Uint8Array([0x06, 0x00, 0x00, 0x00]), // int 6
-  // new Uint8Array([0x07, 0x00, 0x00, 0x00]), // int 7
-  // new Uint8Array([0x08, 0x00, 0x00, 0x00]), // int 8
-  // new Uint8Array([0x09, 0x00, 0x00, 0x00]), // int 9
-  // ];
-  const messages = new Array(8).fill(bytes).map((b) => new Uint8Array(b));
-  // each message in messages must have the same size
-  const hashes = await sha256_gpu(messages);
-  for (let i = 0; i < hashes.length; i += 32) {
-    console.log(
-      hashes
-        .subarray(i, i + 32)
-        .reduce((a, b) => a + b.toString(16).padStart(2, "0"), "")
-    );
-  }
-}
-
-function shader(device: GPUDevice) {
+const shader = (device: GPUDevice) => {
   return /* wgsl */ `
 // SHA-256 for 32-bit aligned messages
 
@@ -170,37 +151,23 @@ fn ch(e: u32, f: u32, g: u32) -> u32 {
   return (e & f) ^ ((~e) & g);
 }
 
-@group(0) @binding(0) var<storage, read_write> messages: array<u32>;
-@group(0) @binding(1) var<storage, read> num_messages: u32;
-@group(0) @binding(2) var<storage, read> message_sizes: array<u32>;
-@group(0) @binding(3) var<storage, read_write> hashes: array<u32>;
+@group(0) @binding(0) var<storage, read_write> block_content: array<u32>;
+@group(0) @binding(1) var<storage, read> block_size: u32;
+@group(0) @binding(2) var<storage, read_write> hashes: array<u32>;
 
 @compute @workgroup_size(${device.limits.maxComputeWorkgroupSizeX})
-fn sha256(@builtin(global_invocation_id) global_id: vec3<u32>) {
+fn mine(@builtin(global_invocation_id) global_id: vec3<u32>) {
 
   let index = global_id.x;
-  if (index >= num_messages) {
-    return;
-  }
+  // if (index >= 1) {
+  //   return;
+  // }
   
-  let message_base_index = index * message_sizes[1];
   let hash_base_index = index * (256u / 32u);
-
-  // == padding == //
-
-  messages[message_base_index + message_sizes[0]] = 0x00000080u;
-  for (var i = message_sizes[0] + 1; i < message_sizes[1] - 2; i++){
-    messages[message_base_index + i] = 0x00000000u;
-  }
-  messages[message_base_index + message_sizes[1] - 2] = 0;
-  messages[message_base_index + message_sizes[1] - 1] = swap_endianess32(message_sizes[0] * 32u);
 
   // == set the nonce == //
 
-  for (var i = 0u; i < 7u; i++) {
-    messages[message_base_index + i] = 0x00000000u;
-  }
-  messages[message_base_index + 7] = swap_endianess32(global_id.x);
+  let nonce_buffer = array<u32,8>(0u, 0u, 0u, 0u, 0u, 0u, 0u, swap_endianess32(index));
 
   // == processing == //
 
@@ -224,12 +191,17 @@ fn sha256(@builtin(global_invocation_id) global_id: vec3<u32>) {
     0x748f82eeu, 0x78a5636fu, 0x84c87814u, 0x8cc70208u, 0x90befffau, 0xa4506cebu, 0xbef9a3f7u, 0xc67178f2u
   );
 
-  let num_chunks = (message_sizes[1] * 32u) / 512u;
+  let num_chunks = (block_size * 32u) / 512u;
   for (var i = 0u; i < num_chunks; i++){
     let chunk_index = i * (512u/32u);
     var w = array<u32,64>();
     for (var j = 0u; j < 16u; j++){
-      w[j] = swap_endianess32(messages[message_base_index + chunk_index + j]);
+      if (i == 0u && j < 8u) {
+        w[j] = swap_endianess32(nonce_buffer[j]);
+      } else {
+        // TODO This swap should possibly be move to the block content initialization where the padding is added
+        w[j] = swap_endianess32(block_content[chunk_index + j - 8]);
+      }
     }
     for (var j = 16u; j < 64u; j++){
       w[j] = w[j - 16u] + g0(w[j - 15u]) + w[j - 7u] + g1(w[j - 2u]);
@@ -272,9 +244,9 @@ fn sha256(@builtin(global_invocation_id) global_id: vec3<u32>) {
   hashes[hash_base_index + 6] = swap_endianess32(hashes[hash_base_index + 6]);
   hashes[hash_base_index + 7] = swap_endianess32(hashes[hash_base_index + 7]);
 }`;
-}
+};
 
-async function getGPUDevice() {
+const getGPUDevice = async () => {
   const adapter = await navigator.gpu.requestAdapter({
     powerPreference: "high-performance",
   });
@@ -285,25 +257,7 @@ async function getGPUDevice() {
   }
 }
 
-// right pads the message to size * 4 bytes with zeros
-function padMessage(bytes: Uint8Array, size: number) {
-  const arrBuff = new ArrayBuffer(size * 4);
-  new Uint8Array(arrBuff).set(bytes);
-  return new Uint32Array(arrBuff);
-}
-
-// [number of 32-bit words, number of 32-bit words padded]
-function getMessageSizes(bytes: Uint8Array) {
-  const lenBit = bytes.length * 8;
-  const k = 512 - ((lenBit + 1 + 64) % 512);
-  const padding = 1 + k + 64;
-  const lenBitPadded = lenBit + padding;
-  return new Uint32Array([lenBit / 32, lenBitPadded / 32]);
-}
-
 function calcNumWorkgroups(device: GPUDevice, messages: Uint8Array[]) {
-  // console.log(device.limits);
-
   const numWorkgroups = Math.ceil(
     messages.length / device.limits.maxComputeWorkgroupSizeX
   );
@@ -316,24 +270,18 @@ function calcNumWorkgroups(device: GPUDevice, messages: Uint8Array[]) {
   return numWorkgroups;
 }
 
-// function check(messages: Uint8Array[]) {
-//   for (const message of messages) {
-//     if (message.length !== messages[0].length)
-//       throw "Messages must have the same size";
-//     if (message.length % 4 !== 0) throw "Message must be 32-bit aligned";
-//   }
-// }
-
 class GPU {
   private device!: GPUDevice;
   private computePipeline!: GPUComputePipeline;
 
   async init() {
     this.device = await getGPUDevice();
+    console.log(this.device.limits);
+
     this.computePipeline = this.device.createComputePipeline({
       compute: {
         module: this.device.createShaderModule({ code: shader(this.device) }),
-        entryPoint: "sha256",
+        entryPoint: "mine",
       },
       layout: "auto",
     });
@@ -351,53 +299,58 @@ class GPU {
 
 let gpu: GPU;
 
-export async function sha256_gpu(messages: Uint8Array[]) {
-  // check(messages);
-
+async function process_block(blockContent: Buffer) {
   gpu = gpu ? gpu : await new GPU().init();
 
   // const numWorkgroups = gpu.getDevice.limits.maxComputeWorkgroupSizeX;
-  const numWorkgroups = calcNumWorkgroups(gpu.getDevice, messages);
+  // const numWorkgroups = calcNumWorkgroups(gpu.getDevice, messages);
+  const workgroupCount = 16;
 
-  const messageSizes = getMessageSizes(messages[0]);
-  const messageArray = new Uint32Array(messageSizes[1] * messages.length);
-  let offset = 0;
-  for (const message of messages) {
-    const messagePad = padMessage(message, messageSizes[1]);
-    // messagePad is the padded version of the input message as described by SHA-256 specification
-    messageArray.set(messagePad, offset);
-    offset += messagePad.length;
+  let offset = blockContent.length;
+
+  const L = (blockContent.length + 32) * 8;
+  const K = 512 - ((L + 1 + 64) % 512);
+  const padding = 1 + K + 64;
+  const Lp = L + padding;
+
+  const paddingBytes = Buffer.alloc(padding / 8);
+  blockContent = Buffer.concat([blockContent, paddingBytes]);
+
+  blockContent.writeUInt8(0x80, offset);
+  offset += 1;
+  for (let i = 1; i < (K + 1) / 8; i++) {
+    blockContent.writeUInt8(0x00, offset);
+    offset += 1;
   }
+  blockContent.writeUInt32BE(0x00, offset);
+  offset += 4;
+  blockContent.writeUInt32BE(L, offset);
+  offset += 4;
 
-  // messages
-  const messageArrayBuffer = gpu.getDevice.createBuffer({
+  // copy blockContent into the buffer
+  const arrBuff = new ArrayBuffer(blockContent.length);
+  new Uint8Array(arrBuff).set(blockContent);
+  const blockContentUint32Buf = new Uint32Array(arrBuff);
+
+  const blockContentBuffer = gpu.getDevice.createBuffer({
     mappedAtCreation: true,
-    size: messageArray.byteLength,
+    size: blockContentUint32Buf.byteLength,
     usage: GPUBufferUsage.STORAGE,
   });
-  new Uint32Array(messageArrayBuffer.getMappedRange()).set(messageArray);
-  messageArrayBuffer.unmap();
+  new Uint32Array(blockContentBuffer.getMappedRange()).set(
+    blockContentUint32Buf
+  );
+  blockContentBuffer.unmap();
 
-  // num_messages
-  const numMessagesBuffer = gpu.getDevice.createBuffer({
+  const blockSizeBuffer = gpu.getDevice.createBuffer({
     mappedAtCreation: true,
     size: Uint32Array.BYTES_PER_ELEMENT,
     usage: GPUBufferUsage.STORAGE,
   });
-  new Uint32Array(numMessagesBuffer.getMappedRange()).set([messages.length]);
-  numMessagesBuffer.unmap();
+  new Uint32Array(blockSizeBuffer.getMappedRange()).set([Lp / 32]);
+  blockSizeBuffer.unmap();
 
-  // message_sizes
-  const messageSizesBuffer = gpu.getDevice.createBuffer({
-    mappedAtCreation: true,
-    size: messageSizes.byteLength,
-    usage: GPUBufferUsage.STORAGE,
-  });
-  new Uint32Array(messageSizesBuffer.getMappedRange()).set(messageSizes);
-  messageSizesBuffer.unmap();
-
-  // Result
-  const resultBufferSize = (256 / 8) * messages.length;
+  const resultBufferSize = (256 / 8) * workgroupCount;
   const resultBuffer = gpu.getDevice.createBuffer({
     size: resultBufferSize,
     usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
@@ -409,23 +362,17 @@ export async function sha256_gpu(messages: Uint8Array[]) {
       {
         binding: 0,
         resource: {
-          buffer: messageArrayBuffer,
+          buffer: blockContentBuffer,
         },
       },
       {
         binding: 1,
         resource: {
-          buffer: numMessagesBuffer,
+          buffer: blockSizeBuffer,
         },
       },
       {
         binding: 2,
-        resource: {
-          buffer: messageSizesBuffer,
-        },
-      },
-      {
-        binding: 3,
         resource: {
           buffer: resultBuffer,
         },
@@ -438,7 +385,7 @@ export async function sha256_gpu(messages: Uint8Array[]) {
   const passEncoder = commandEncoder.beginComputePass();
   passEncoder.setPipeline(gpu.getComputePipeline);
   passEncoder.setBindGroup(0, bindGroup);
-  passEncoder.dispatchWorkgroups(numWorkgroups);
+  passEncoder.dispatchWorkgroups(workgroupCount);
   passEncoder.end();
 
   const gpuReadBuffer = gpu.getDevice.createBuffer({
